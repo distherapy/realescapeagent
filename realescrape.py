5e28362cc20d275b6e7cf5ab7525ae3838d4faa5
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-01 18:37:29 -0500

    Create realescrape.py

---
+++ realescrape.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import requests
+import scrapy
+import re
+#pseudolayout
+#
+#ask user which county and state
+#ask user commercial, residential, county plat map
+#
+faceamount = response.xpath('faceValue')
+print(faceamount)
+
+x = raw_input("Which county and which state are you looking?")
+y = raw_input("Enter 'C' or 'R' for Commericial, Residential")
+
+allowed_domains = ["arizonataxsale.com"]
+
+class taxLien(scrapy.Spider):
+    name = "taxlien"
+
+    def start_requests(self):
+        urls = ['https://apache.arizonataxsale.com/index.cfm?folder=previewitems',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # def parse(self, response):
+        # page = response.url.split("/")[-2]
+        # filename = f'taxlien-{page}.html'
+        # with open(filename, 'wb') as f:
+            # f.write(response.body)
+        # self.log(f'Saved file {filename}')
+        
+    def parse(self, response):
+        available = response.xpath('//*[@id="previewResults"]/table//tr')
+        for debts in available[1:]:
+            item['debt'] = product.xpath('td[faceValue]//text()').extract_first()
+            yield item
+
+url = "https://apache.arizonataxsale.com/index.cfm?folder=previewitems"
+
+r = requests.get(url)
+df_list = pd.read_html(r.text)
+df = df_list[0]
+df.head()
+print(df)
