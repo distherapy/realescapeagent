f38284d2ebb4f22eb02a49bb88766eab1a9f527d
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-10 11:29:58 -0500

    Update realescrape.py

--- realescrape.py
+++ realescrape.py
@@ -5,42 +5,67 @@ import re
 #pseudolayout
 #
 #ask user which county and state
-#ask user commercial, residential, county plat map
+#ask user the options --> commercial, residential, lot size, house size, price min-max, mls number, county, state)
 #
+#realtor, zillow, mls, landzero, landsofamerica, landflip, propertyshark, trulia, redfin, movoto, century21, homes.com
+#
+com = input("Type 'com' for commercial or 'res' for residential:")
+res = com
+county = input("Type the name of the county i.e. 'Essex':")
+state = input("Type the initials for the state i.e. 'NJ':")
+state_full = state.state(full)
+pmin = input("Type the minimum price digits only i.e. '35000':")
+pmax = input("Type the maximum price digits only i.e. '125000':")
+lmin = input("Type the minimum acreage in sq feet (1 acre is 43560 square feet):")
+lmax = input("Type the maximum acreage in sq feet (50 acres equals 2178000 square feet):")
+
+def realtor(com, res, hmin, hmax, lmin, pmin, pmax, county, state):
+	url = 'https://www.realtor.com/realestateandhomes-search/'+ county + '-' + 'County_' + state + '/price-' + pmin + '-' + pmax + '/lot-sqft-' + lmin
+	r = requests.get(url)
+	df_list = pd.read_html(r.text)
+	df = df_list[0]
+	df.head()
+	print(df)
+
+def mls(mls, com, res, hmin, hmax, lmin, pmin, pmax, county, state):
+	url = 'https://www.mls.com/'+ mls'
+	r = requests.get(url)
+	df_list = pd.read_html(r.text)
+	df = df_list[0]
+	df.head()
+	print(df)
+
+def zillow(hmin, hmax, lmin, pmin, pmax, county, state):
+	url = 'https://www.zillow.com/realestateandhomes-search/'+ county + '-' + 'County_' + state + '/price-' + pmin + '-' + pmax + '/lot-sqft-' + lmin
+	r = requests.get(url)
+	df_list = pd.read_html(r.text)
+	df = df_list[0]
+	df.head()
+	print(df)
+
+def landzero(pmin, pmax, county, state):
+	url = 'https://www.landzero.com/land-for-sale/' + pmin + '-to-' + pmax'
+	r = requests.get(url)
+	df_list = pd.read_html(r.text)
+	df = df_list[0]
+	df.head()
+	print(df)
+	
+def landflip(county, state_full, pmin, pmax):
+	url = 'https://www.landflip.com/land-fsbo/' + state + '/' + pmin +'-minprice/' + pmax +'-maxprice/
+	r = requests.get(url)
+	df_list = pd.read_html(r.text)
+	df = df_list[0]
+	df.head()
+	print(df)
+
+def propertyshark(county, state, pmin, pmax):
+	url = 'https://www.propertyshark.com/homes/US/Real-Estate-Listings/' + state + '.html?location=' + state +'-minprice/' + pmax +'-maxprice/
+	r = requests.get(url)
+	df_list = pd.read_html(r.text)
+	df = df_list[0]
+	df.head()
+	print(df)
+'''
 faceamount = response.xpath('faceValue')
 print(faceamount)
-
-x = raw_input("Which county and which state are you looking?")
-y = raw_input("Enter 'C' or 'R' for Commericial, Residential")
-
-allowed_domains = ["arizonataxsale.com"]
-
-class taxLien(scrapy.Spider):
-    name = "taxlien"
-
-    def start_requests(self):
-        urls = ['https://apache.arizonataxsale.com/index.cfm?folder=previewitems',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    # def parse(self, response):
-        # page = response.url.split("/")[-2]
-        # filename = f'taxlien-{page}.html'
-        # with open(filename, 'wb') as f:
-            # f.write(response.body)
-        # self.log(f'Saved file {filename}')
-        
-    def parse(self, response):
-        available = response.xpath('//*[@id="previewResults"]/table//tr')
-        for debts in available[1:]:
-            item['debt'] = product.xpath('td[faceValue]//text()').extract_first()
-            yield item
-
-url = "https://apache.arizonataxsale.com/index.cfm?folder=previewitems"
-
-r = requests.get(url)
-df_list = pd.read_html(r.text)
-df = df_list[0]
-df.head()
-print(df)
